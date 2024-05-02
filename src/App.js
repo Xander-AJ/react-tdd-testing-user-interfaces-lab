5c4781eb738bc6930159197240c8afcb7539d609
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-05-02 08:17:20 +0300

    complete lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+
+import React from "react";
+
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm (your name)</h1>
+      <img src="https://media.licdn.com/dms/image/D4D35AQHyUoyDahd99Q/profile-framedphoto-shrink_400_400/0/1704949500964?e=1715184000&v=beta&t=fnbKxxlO_t9oyKrUt4lnlRm-Lo1QCdhxs-OrvbNrolY" alt="John-Alexander Kamau" />
+      <h2>About Me</h2>
+      <p>Biography here</p>
+      <a href="https://github.com/Xander-AJ">GitHub</a>
+      <a href="https://www.linkedin.com/in/john-alexander-kamau-301821a5/">LinkedIn</a>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,88 @@
+// src/__tests__/App.test.js
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom';
 
 import App from "../App";
 
-// Your tests here
+// Test cases
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+  // Arrange
+  render(<App />);
+
+  // Act
+  const topLevelHeading = screen.getByRole("heading", {
+    name: /hi, i'm/i,
+    exact: false,
+    level: 1,
+  });
+
+  // Assert
+  expect(topLevelHeading).toBeInTheDocument();
+});
+
+test("displays an image of yourself with alt text", () => {
+  // Arrange
+  render(<App />);
+
+  // Act
+  const image = screen.getByAltText(/John-Alexander Kamau/i);
+
+  // Assert
+  expect(image).toBeInTheDocument();
+});
+
+test("displays a second-level heading with the text 'About Me'", () => {
+  // Arrange
+  render(<App />);
+
+  // Act
+  const secondLevelHeading = screen.getByRole("heading", {
+    name: /about me/i,
+    exact: false,
+    level: 2,
+  });
+
+  // Assert
+  expect(secondLevelHeading).toBeInTheDocument();
+});
+
+test("displays a paragraph for your biography", () => {
+  // Arrange
+  render(<App />);
+
+  // Act
+  const bioParagraph = screen.getByText(/biography/i);
+
+  // Assert
+  expect(bioParagraph).toBeInTheDocument();
+});
+
+test("displays a link to your GitHub page", () => {
+  // Arrange
+  render(<App />);
+
+  // Act
+  const githubLink = screen.getByRole("link", {
+    name: /github/i,
+    exact: false,
+  });
+
+  // Assert
+  expect(githubLink).toBeInTheDocument();
+  expect(githubLink).toHaveAttribute("href", "https://github.com/Xander-AJ");
+});
+
+test("displays a link to your LinkedIn page", () => {
+  // Arrange
+  render(<App />);
+
+  // Act
+  const linkedinLink = screen.getByRole("link", {
+    name: /linkedin/i,
+    exact: false,
+  });
+
+  // Assert
+  expect(linkedinLink).toBeInTheDocument();
+  expect(linkedinLink).toHaveAttribute("href", "https://www.linkedin.com/in/john-alexander-kamau-301821a5/");
+});
